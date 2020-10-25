b7858aef0587e3c597619c6c16d60918f604f479
cvalouxis <cvalouxis@gmail.com>
2020-10-25 16:50:41 +0200

    p6

--- p6.py
+++ p6.py
@@ -5,14 +5,11 @@ Identity Matrix
 An identity matrix is defined as a square matrix with 1s running 
 from the top left of the square to the bottom right. 
 The rest are 0s. 
-The identity matrix has applications ranging from machine learning 
-to the general theory of relativity.
 
 Create a function that takes an integer n and returns 
 the identity matrix of n x n dimensions. 
 For this challenge, if the integer is negative, return the mirror 
 image of the identity matrix of n x n dimensions. 
-It does not matter if the mirror image is left-right or top-bottom.
 
 Examples
 id_mtrx(2) ➞ [
